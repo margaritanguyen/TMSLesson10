f3e666859393b3d1a6e06c67f79cf76576e01c22
Margarita <40401383+margaritanguyen@users.noreply.github.com>
2023-03-01 19:35:39 +0300

    init commit

---
+++ TMSLesson10/Credentials.cs
@@ -0,0 +1,29 @@
+﻿namespace TMSLesson10
+{
+    class Credentials
+    {
+        public static bool IsValid(string login, string password, string confirmPassword)
+        {
+            try
+            {
+                if (login.Length > 20 || login.Contains(' '))
+                    throw new WrongLoginException("Логин не соответствует требованиям");
+
+                if (password.Length > 20 || password.Contains(' ') || !password.Any(char.IsDigit))
+                    throw new WrongPasswordException("Пароль не соответствует требованиям");
+
+                if (!password.Equals(confirmPassword))
+                    throw new WrongPasswordException("Пароли не совпадают");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ TMSLesson10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TMSLesson10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Credentials.IsValid("morethantwentysymbols", "123qweASD", "123qweASD")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login with spaces", "123qweASD", "123qweASD")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login", "morethantwentysymbols", "123qweASD")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login", "pass word", "123qweASD")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login", "withoutdigits", "123qweASD")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login", "123qweASD", "123QWEasd")) Console.WriteLine("Все ОК");
+            if (Credentials.IsValid("login", "123qweASD", "123qweASD")) Console.WriteLine("Все ОК");
+        }
+    }
+}
